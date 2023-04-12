6fe3100be6a0586b7b61516e5dad98a150f918dd
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-04-12 18:40:42 -0500

    modificacion de modelo reserva

--- back/src/models/Reserva.js
+++ back/src/models/Reserva.js
@@ -4,26 +4,40 @@ module.exports = (sequelize) => {
   sequelize.define(
     "reserva",
     {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
       date: {
-        type: DataTypes.DATEONLY,
+        type: DataTypes.DATE,
         allowNull: false,
       },
       start: {
-        type: DataTypes.TIME,
+        type: DataTypes.STRING,
         allowNull: false,
       },
       end: {
-        type: DataTypes.TIME,
-        allowNull: false,
-      },
-      status: {
         type: DataTypes.STRING,
         allowNull: false,
-        defaultValue: "pending",
       },
-      hasPromo: {
+      canchaid: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      userid: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      promo: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
+      },
+      confirmed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
       },
     },
     { paranoid: true }
